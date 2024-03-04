466cce8673b314ea963da58f4549e0b75e6815c1
Gamid-FrontEnd <g1a6m0i9d@gmail.com>
2024-03-04 22:36:42 +0200

    add some improvements to HomePage.tsx

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -36,14 +36,67 @@ const Intro = styled.div`
   transform: translate(-50%, -50%);
 `;
 
+const Menu = styled.div`
+  position: absolute;
+  top: 70%;
+  left: 30%;
+  transform: translate(-50%, -50%);
+`;
+
+//  edit styles for active later
+const Item = styled.li`
+  cursor: pointer;
+  transition: all 0.4s;
+  color: #28d4f7;
+
+  &:hover {
+    transform: scale(1.2);
+    color: #0d9aff;
+  }
+
+  &:active {
+    color: white;
+  }
+`;
+
+const UnorderedList = styled.ul`
+  list-style: none;
+
+  ${Item} {
+    margin-bottom: 10px;
+  }
+`;
+
+const H1 = styled.h1`
+  font-family: "Anta", sans-serif;
+  margin: 0;
+`;
+
+const H3 = styled(H1)`
+  font-size: 20px;
+`;
+
 const HomePage: React.FC = () => {
   return (
     <div className="homepage_main_div">
       <LeftBlock>
         <Intro>
-          <h1>Hamid Akhmedov</h1>
-          <h3>Frontend developer</h3>
+          <H1>Name Surname</H1>
+          <H3>Frontend developer</H3>
         </Intro>
+        <Menu>
+          <UnorderedList>
+            <Item>
+              <a>About</a>
+            </Item>
+            <Item>
+              <a>Projects</a>
+            </Item>
+            <Item>
+              <a>Contact</a>
+            </Item>
+          </UnorderedList>
+        </Menu>
       </LeftBlock>
       <RightBlock></RightBlock>
     </div>
